52a28a57141253412e83c956b4b9978ab716d41c
David Steele <david@pgmasters.net>
2015-07-28 15:14:06 -0400

    Logging audit fields.

--- analyze/sql/audit.sql
+++ analyze/sql/audit.sql
@@ -29,7 +29,8 @@ create table audit_log.session
     state text not null
         constraint session_state_ck check (state in ('ok', 'error')),
 
-    constraint session_pk primary key (id),
+    constraint session_pk
+        primary key (id),
     constraint session_processid_sessionkey_sessionstarttime_unq
         unique (process_id, session_key, session_start_time)
 );
@@ -41,12 +42,27 @@ create table audit_log.session
 --     constraint logon_pk primary key (id)
 -- )
 
-create table audit_log.event
+create table audit_log.log_event
 (
     session_id bigint not null
         constraint even_sessionid_fk
             references audit_log.session (id),
-    timestamp timestamp(3) with time zone not null,
+    session_line_num bigint not null,
+    log_time timestamp(3) with time zone not null,
+    command text,
+    message text,
+    query text,
+
+    constraint logevent_pk
+        primary key (session_id, session_line_num)
+    -- constraint logevent_sessionid_sessionlinenum_unq
+    --     unique (session_id, session_line_num)
+);
+
+create table audit_log.audit_event
+(
+    session_id bigint not null,
+    session_line_num bigint not null,
     audit_type text not null
         constraint event_audittype_ck
             check (audit_type in ('s', 'o')),
@@ -61,5 +77,11 @@ create table audit_log.event
     object_type text,
     object_name text,
     statement text,
-    parameter text[]
+    parameter text[],
+
+    constraint auditevent_pk
+        primary key (session_id, session_line_num),
+    constraint auditevent_sessionid_sessionlinenum_fk
+        foreign key (session_id, session_line_num)
+        references audit_log.log_event (session_id, session_line_num)
 );
