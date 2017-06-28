d0fb52d446ed10cafe304aed16b193c1803cd906
David Steele <david@crunchydata.com>
2017-06-28 11:22:01 -0400

    Bump 9.5 extension version to 1.0.6.
    The 9.5 version of the extension was accidentally left at 1.0 which does not reflect the various updates. The default is now set to 1.0.6 and an empty update script is included. Run:
    
    alter extension pgaudit update to '1.0.6'
    
    to update the extension version to 1.0.6, though there are no apparent problems with leaving it at 1.0.

---
+++ pgaudit--1.0--1.0.6.sql
@@ -0,0 +1,6 @@
+/* pgaudit/pgaudit--1.0--1.0.6.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pgaudit" to load this file.\quit
+
+-- nothing to do, this corrects the initially incorrect version for 9.5
