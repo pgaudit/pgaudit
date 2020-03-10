6ecab517a43b095cd3ef02b6079d48388f1325d6
Peter Eisentraut <peter@eisentraut.org>
2020-03-10 14:06:53 +0100

    Differentiate UPDATE from SELECT FOR UPDATE queries
    When auditing write operations (pgaudit.log = 'write'), we check
    whether the query requires ACL_UPDATE permissions.  This also catches
    SELECT FOR UPDATE, SELECT FOR KEY SHARE, etc. queries, because there
    is no separate permission for those.  This is especially annoying
    because internally generated foreign-key queries get logged.
    
    To avoid this, in PostgreSQL 13 we can also check the rellockmode
    field of the range table entry.  If it's at least RowExclusiveLock,
    then it's a real UPDATE, else it's some kind of SELECT with locking
    clause.

--- pgaudit.c
+++ pgaudit.c
@@ -1033,7 +1033,9 @@ log_select_dml(Oid auditOid, List *rangeTabls)
         /*
          * We don't have access to the parsetree here, so we have to generate
          * the node type, object type, and command tag by decoding
-         * rte->requiredPerms and rte->relkind.
+         * rte->requiredPerms and rte->relkind.  For updates we also
+         * check rellockmode so that only true UPDATE commands but not
+         * SELECT FOR UPDATE etc. are classified.
          */
         if (rte->requiredPerms & ACL_INSERT)
         {
@@ -1041,7 +1043,8 @@ log_select_dml(Oid auditOid, List *rangeTabls)
             auditEventStack->auditEvent.commandTag = T_InsertStmt;
             auditEventStack->auditEvent.command = COMMAND_INSERT;
         }
-        else if (rte->requiredPerms & ACL_UPDATE)
+        else if (rte->requiredPerms & ACL_UPDATE &&
+                 rte->rellockmode >= RowExclusiveLock)
         {
             auditEventStack->auditEvent.logStmtLevel = LOGSTMT_MOD;
             auditEventStack->auditEvent.commandTag = T_UpdateStmt;
