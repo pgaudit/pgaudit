99e8cfac264909c7dd6fb45a1e3c71a8dacc0b26
David Steele <david@pgmasters.net>
2021-11-04 18:18:42 -0400

    Remove unneeded version comments.

--- pgaudit--1.0--1.0.6.sql
+++ pgaudit--1.0--1.0.6.sql
@@ -1,5 +1,3 @@
-/* pgaudit/pgaudit--1.0--1.0.6.sql */
-
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pgaudit" to load this file.\quit
 
--- pgaudit--1.0.7--1.0.8.sql
+++ pgaudit--1.0.7--1.0.8.sql
@@ -1,4 +1,2 @@
-/* pgaudit/pgaudit--1.0.7--1.0.8.sql */
-
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pgaudit" to load this file.\quit
