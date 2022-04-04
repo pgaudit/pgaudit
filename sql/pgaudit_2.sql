27a3e20f47785368cca6fe7a7836264edd62398b
tjjcarroll <tjjcarroll@hotmail.com>
2022-04-04 20:06:21 +0000

    Ready for pull request.

--- sql/pgaudit_2.sql
+++ sql/pgaudit_2.sql
@@ -27,52 +27,59 @@ CREATE TABLE role_test
     id integer 
 ); -- not logged
 
-SELECT * FROM role_test; -- not logged
+SELECT * FROM role_test; -- not audited
 
-INSERT INTO role_test values (1); -- logged session
+INSERT INTO role_test values (1); -- session audited
 
 
 
 -- Test 2
 -- ROLE-BASED - new role-based (postgres superuser a member of all roles)
-RESET pgaudit.log; -- not logged
-CREATE USER roleuser1 password 'password'; -- not logged
-SET pgaudit.roles_scope = 'roleuser1';  -- logged on set
+RESET pgaudit.log; -- not audited
+CREATE USER roleuser1 password 'password'; -- not audited
+CREATE ROLE roleuser2 LOGIN password 'password';   -- not audited
+SET pgaudit.roles_scope = 'roleuser1';  -- role-based audited on set
 
-SELECT * FROM role_test; -- logged role
+SELECT * FROM role_test; -- role-based audited
 
 -- Test 3
 -- Superuser changes ROLE to less privileged
-GRANT SELECT ON role_test TO roleuser1;  -- logged role
-set role 'roleuser1';  -- logged role
+GRANT SELECT ON role_test TO roleuser1;  -- role-based audited
+GRANT SELECT ON role_test TO roleuser2;  -- role-based audited
+set role 'roleuser1';  -- role-based audited
 
-SELECT * FROM role_test; -- logged role
+SELECT * FROM role_test; -- role-based audited
+
+SET SESSION AUTHORIZATION 'roleuser2'; -- role-based audited
+
+SELECT * FROM role_test; -- role-based audited
 
 
 -- Test 4
 -- Non-logged Role - session auditing not impacted
 \connect - :current_user
-CREATE ROLE roleuser2 LOGIN password 'password';   -- not logged
-ALTER ROLE roleuser2 SET pgaudit.log = 'write';  -- not logged
-ALTER ROLE roleuser2 SET pgaudit.log_client = ON; -- not logged
-ALTER ROLE roleuser2 SET pgaudit.log_level = 'warning'; -- not logged
-ALTER ROLE roleuser2 SET pgaudit.roles_scope = 'roleuser1'; -- not logged
-GRANT SELECT, INSERT ON role_test TO roleuser2; -- not logged
+ALTER ROLE roleuser2 SET pgaudit.log = 'write';  -- not audited
+ALTER ROLE roleuser2 SET pgaudit.log_client = ON; -- not audited
+ALTER ROLE roleuser2 SET pgaudit.log_level = 'warning'; -- not audited
+ALTER ROLE roleuser2 SET pgaudit.roles_scope = 'roleuser1'; -- not audited
+GRANT SELECT, INSERT ON role_test TO roleuser2; -- not audited
+
+
 
 \connect - roleuser2
 
-INSERT INTO role_test values (2); -- logged session
-SELECT * FROM role_test; -- not logged
+INSERT INTO role_test values (2); -- session audited
+SELECT * FROM role_test; -- not audited
 
 -- Test 5
 -- Membership of Role in scope for auditing
 \connect - :current_user
-GRANT roleuser1 TO roleuser2; -- not logged
+GRANT roleuser1 TO roleuser2; -- not audited
 
 \connect - roleuser2
 
-INSERT INTO role_test values (3); -- logged session (session takes precedence)
-SELECT * FROM role_test; -- logged role
+INSERT INTO role_test values (3); -- session audited (session takes precedence)
+SELECT * FROM role_test; -- role-based audited
 
 
 
@@ -84,21 +91,21 @@ SELECT * FROM role_test; -- logged role
 CREATE TABLE role_test2
 (
 	id INT
-); -- not logged
+); -- not audited
 
 
 CREATE TABLE role_test3
 (
 	id INT
-); -- not logged
+); -- not audited
 
-REVOKE roleuser1 from roleuser2; -- not logged
-GRANT SELECT, INSERT ON role_test2 to roleuser2; -- not logged
-GRANT INSERT ON role_test3 to roleuser2; -- not logged
+REVOKE roleuser1 from roleuser2; -- not audited
+GRANT SELECT, INSERT ON role_test2 to roleuser2; -- not audited
+GRANT INSERT ON role_test3 to roleuser2; -- not audited
 
 
-ALTER ROLE roleuser2 SET pgaudit.log_parameter_for_role_based = ON; -- not logged
-ALTER ROLE roleuser2 SET pgaudit.log = 'read'; -- not logged
+ALTER ROLE roleuser2 SET pgaudit.log_parameter_for_role_based = ON; -- not audited
+ALTER ROLE roleuser2 SET pgaudit.log = 'read'; -- not audited
 
 CREATE FUNCTION roletest2_insert() RETURNS TRIGGER AS $$
 BEGIN
@@ -107,16 +114,16 @@ BEGIN
  WHERE id = new.id;
 
  RETURN new;
-END $$ LANGUAGE plpgsql security definer; -- not logged
+END $$ LANGUAGE plpgsql security definer; -- not audited
 
-SET pgaudit.log_client = ON; -- not logged
-SET pgaudit.log_level = 'notice'; -- not logged
-SET pgaudit.roles_scope = 'roleuser1';  -- logged on set
+SET pgaudit.log_client = ON; -- not audited
+SET pgaudit.log_level = 'notice'; -- not audited
+SET pgaudit.roles_scope = 'roleuser1';  -- role-based audited on set
 
 
 CREATE TRIGGER roletest2_insert_trg
 	AFTER INSERT ON role_test2
-	FOR EACH ROW EXECUTE PROCEDURE roletest2_insert();  -- logged role
+	FOR EACH ROW EXECUTE PROCEDURE roletest2_insert();  -- role-based audited
 
 
 
@@ -128,29 +135,30 @@ WITH CTE AS
 )
 INSERT INTO role_test2
 SELECT id
-  FROM cte; -- Logged role, insert and trigger based update
+  FROM cte; -- role-based audit: insert and trigger-based update
 
 --Test 6 & 7 Execution
 \connect - roleuser2
 
-INSERT INTO role_test2 VALUES (2); -- initial insert not logged, trigger based update logged with parameter
+INSERT INTO role_test2 VALUES (2); -- initial insert not audited, trigger-based update role-based audited with parameter
 
-INSERT INTO role_test3 VALUES(2);  -- not logged
+INSERT INTO role_test3 VALUES(2);  -- not audited
 
-SELECT * FROM role_test2;  -- logged session
+SELECT * FROM role_test2;  -- session audited
 
+
+--Test 8 - no parameters
 \connect - :current_user
-CREATE ROLE roleuser3 LOGIN password 'password'; -- not logged
-GRANT SELECT, UPDATE ON role_test2 to roleuser3; -- not logged
-ALTER FUNCTION roletest2_insert() OWNER TO roleuser3; -- not logged
+CREATE ROLE roleuser3 LOGIN password 'password'; -- not audited
+GRANT SELECT, INSERT, UPDATE ON role_test2 TO roleuser3; -- not audited
+GRANT SELECT, INSERT on role_test3 TO roleuser2; -- not audited
+ALTER FUNCTION roletest2_insert() OWNER TO roleuser3; -- not audited
 
-ALTER ROLE roleuser2 SET pgaudit.log_parameter_for_role_based = OFF
-
---<sort out here - want to confirm that parameteres not logged on insert>
-ALTER ROLE roleuser2 SET pgaudit.log = 'read, write'; -- not logged
+ALTER ROLE roleuser2 SET pgaudit.log_parameter_for_role_based = OFF; -- not audited
+ALTER ROLE roleuser2 SET pgaudit.log = 'read, write'; -- not audited
 
 \connect - roleuser2
-INSERT INTO role_test2 VALUES (3); -- not logged <session logged without param>
+INSERT INTO role_test2 VALUES (3); -- session audited without param
 
 WITH CTE AS
 (
@@ -159,62 +167,62 @@ WITH CTE AS
 )
 INSERT INTO role_test2
 SELECT id
-  FROM cte; 
+  FROM cte; -- session audited without param
 
 
-
---Test 8 - DO with security definer
+--Test 9 - DO with security definer
 \connect - :current_user
-SET pgaudit.log_client = ON; -- not logged
-SET pgaudit.log_level = 'notice'; -- not logged
-SET pgaudit.roles_scope = 'roleuser1';  -- logged on set
+SET pgaudit.log_client = ON; -- not audited
+SET pgaudit.log_level = 'notice'; -- not audited
+SET pgaudit.roles_scope = 'roleuser1';  -- role-based audited on set
 
 CREATE FUNCTION roletest2_change(change_id int) RETURNS void AS $$
 BEGIN
  UPDATE role_test2
   SET id = id + 1
   WHERE id = change_id;
-END $$ LANGUAGE plpgsql security definer; -- logged role
+END $$ LANGUAGE plpgsql security definer; -- role-based audited
 
-DO $$ BEGIN PERFORM roletest2_change(91); END $$; -- logged role, DO, SELECT, EXECUTE, UPDATE
+DO $$ BEGIN PERFORM roletest2_change(91); END $$; -- role-based audited: DO, SELECT, EXECUTE, UPDATE
 
 
-GRANT SELECT, INSERT, UPDATE ON role_test2 to roleuser2; -- logged role
-ALTER FUNCTION roletest2_change(int) OWNER TO roleuser3; -- logged role
+GRANT SELECT, INSERT, UPDATE ON role_test2 to roleuser2; -- role-based audited
+ALTER FUNCTION roletest2_change(int) OWNER TO roleuser3; -- role-based audited
 
 
 
 \connect - roleuser2
-DO $$ BEGIN PERFORM roletest2_change(93); END $$; -- logged session for select
+DO $$ BEGIN PERFORM roletest2_change(93); END $$; -- session audit for select
 
 
---Test 9 - Extension role based (order of execution)
+--Test 10 - Extension role based (order of execution)
 \connect - :current_user
-SET pgaudit.log_client = ON; -- not logged
-SET pgaudit.log_level = 'notice'; -- not logged
-ALTER SYSTEM SET pgaudit.roles_scope = 'roleuser1';  -- logged on set
-
-ALTER EXTENSION pgaudit UPDATE; -- logged role
-
---Test 10 - Close Portal role based (order of execution)
-CLOSE ALL;
+SET pgaudit.log_client = ON; -- not audited
+SET pgaudit.log_level = 'notice'; -- not audited
+ALTER SYSTEM SET pgaudit.roles_scope = 'roleuser1';   -- not audited
+SELECT pg_reload_conf(); -- not audited
 
 
-ALTER ROLE roleuser2 SET pgaudit.roles_scope = 'roleuser2';
-ALTER ROLE roleuser2 RESET pgaudit.log;
+ALTER EXTENSION pgaudit UPDATE; -- role-based audited
+
+--Test 11 - Close Portal role based (order of execution)
+CLOSE ALL; -- role-based audited
+
+--Test 12 - user is one of role scope items
+ALTER ROLE roleuser2 SET pgaudit.roles_scope = 'roleuser1, roleuser2'; -- role-based audited
+ALTER ROLE roleuser2 RESET pgaudit.log; -- role-based audited
 
 
 \connect - roleuser2;
-select * from role_test; -- logged role
+select * from role_test; -- role-based audited
 
 
 
 \connect - :current_user
-DROP FUNCTION roletest2_insert();
+DROP FUNCTION roletest2_insert CASCADE;
 DROP FUNCTION roletest2_change;
-DROP TRIGGER roletest2_insert_trg on role_test2;
 
-REVOKE roleuser1 from roleuser2;
+
 REVOKE ALL ON TABLE role_test FROM roleuser1;
 REVOKE ALL ON TABLE role_test FROM roleuser2;
 REVOKE ALL ON TABLE role_test2 FROM roleuser2;
@@ -226,3 +234,5 @@ DROP USER roleuser3;
 DROP TABLE public.role_test;
 DROP TABLE public.role_test2;
 DROP TABLE public.role_test3;
+ALTER SYSTEM SET pgaudit.roles_scope = '[none]';
+SELECT pg_reload_conf(); 
