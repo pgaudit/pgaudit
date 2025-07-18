343547e32b590233d20384ef5159ae6c9c9411c6
David Steele <david@pgbackrest.org>
2025-07-18 16:58:00 -0400

    Update parameter in ExecutorCheckPerms hook.
    The abort parameter conflicts with the abort function so rename it to the name used in the hook type.

--- pgaudit.c
+++ pgaudit.c
@@ -1489,7 +1489,9 @@ pgaudit_ExecutorStart_hook(QueryDesc *queryDesc, int eflags)
  * Hook ExecutorCheckPerms to do session and object auditing for DML.
  */
 static bool
-pgaudit_ExecutorCheckPerms_hook(List *rangeTabls, List *permInfos, bool abort)
+pgaudit_ExecutorCheckPerms_hook(List *rangeTabls,
+                                List *permInfos,
+                                bool ereport_on_violation)
 {
     Oid auditOid;
 
@@ -1536,7 +1538,8 @@ pgaudit_ExecutorCheckPerms_hook(List *rangeTabls, List *permInfos, bool abort)
 
     /* Call the next hook function */
     if (next_ExecutorCheckPerms_hook &&
-        !(*next_ExecutorCheckPerms_hook) (rangeTabls, permInfos, abort))
+        !(*next_ExecutorCheckPerms_hook)(rangeTabls, permInfos,
+                                         ereport_on_violation))
         return false;
 
     return true;
